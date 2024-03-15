1852559e2bc47009c751308499d252917931eba8
Onizuka121 <134316745+Onizuka121@users.noreply.github.com>
2024-03-15 06:02:45 +0100

    Registrazione pagina

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function ChangePage(page_path){
     window.location = page_path;
     console.log("work");
 }
+
+function HandleAndView(div_to_handle,div_to_view){
+    document.getElementById(div_to_handle).style.display = "none";
+    document.getElementById(div_to_view).style.display = "block";
+}
