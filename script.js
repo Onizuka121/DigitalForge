6ec17794c28287e78906984f40aa5ae3870898f5
Onizuka121 <134316745+Onizuka121@users.noreply.github.com>
2024-03-26 06:12:20 +0100

    Big Commit

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function ChangePage(page_path){
     window.location = page_path;
-    console.log("work");
 }
 
 function HandleAndView(div_to_handle,div_to_view){
     document.getElementById(div_to_handle).style.display = "none";
     document.getElementById(div_to_view).style.display = "block";
 }
+$(document).ready(function () {
+    if (!$.browser.webkit) {
+        $('.wrapper').html('<p>Sorry! Non webkit users. :(</p>');
+    }
+});
