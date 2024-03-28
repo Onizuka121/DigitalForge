2b2a44ca40c96c0bf2dcf4b1a4d25832d77f4ccc
Onizuka121 <134316745+Onizuka121@users.noreply.github.com>
2024-03-28 08:51:33 +0100

    Update Market-place and fixed many bugs

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
+
+
+let id_divs_nav_bar = [
+    "li-mk-ID",
+    "li-ai-ID",
+    "li-pre-ID",
+    "li-aiuto-ID"
+];
+
+let div_setted_id = "mkplaceID";
+
+ColorOrNotNavItem("li-mk-ID");
+
 function ChangePage(page_path){
     window.location = page_path;
 }
 
-function HandleAndView(div_to_handle,div_to_view){
-    document.getElementById(div_to_handle).style.display = "none";
-    document.getElementById(div_to_view).style.display = "block";
+function HandleAndView(div_to_handle,div_to_color = undefined){
+   // document.getElementById(div_to_handle).style.display = "none";
+    // document.getElementById(div_to_view).style.display = "block";
+    ColorOrNotNavItem(div_to_color);
+    
 }
-$(document).ready(function () {
-    if (!$.browser.webkit) {
-        $('.wrapper').html('<p>Sorry! Non webkit users. :(</p>');
-    }
-});
+
+
+function ColorOrNotNavItem(id_div_nav_bar){
+    id_divs_nav_bar.forEach(element => {
+        if(element != id_div_nav_bar){
+            document.getElementById(element).style.cssText = ".nav-item:hover{ background-color: #21252937; cursor: pointer; }";
+        }
+    });
+    document.getElementById(id_div_nav_bar).style.backgroundColor = "#212529";
+}
+
